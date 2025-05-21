4844faa6e8849d875aba17e9ca3b66526d8988e9
jvdy710 <judyp12@nycstudents.net>
2025-05-21 11:39:11 -0400

    Delete Frqone.java

--- Frqone.java
+++
@@ -1,5 +0,0 @@
-public int walkDogs(int hour){
-        int dogsToWalk = Math.min(maxDogs, company.numAvailableDogs(hour));
-        company.updateDogs(hour, dogsToWalk);
-        return dogsToWalk;
-    }
